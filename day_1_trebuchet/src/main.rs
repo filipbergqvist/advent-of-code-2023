82ac85ac7a598d09a8adc83255709f9c9b3b2e84
Filip Bergqvist <filip.0717@hotmail.com>
2023-12-02 21:38:59 +0100

    * Very inneficient, but working solution for Part 2

--- day_1_trebuchet/src/main.rs
+++ day_1_trebuchet/src/main.rs
@@ -1,9 +1,10 @@
 use std::fs;
 use std::str;
 
+const INPUT_PATH: &str = "res/input.txt";
+
 fn main() {
-    let path: &str = "res/input.txt";
-    let input: String = fs::read_to_string(path)
+    let input: String = fs::read_to_string(INPUT_PATH)
         .expect("Fail");
 
     // we want to collect the calibration value of each line
@@ -15,17 +16,38 @@ fn main() {
     // iterate over the lines
     for line in lines {
 
+        println!("\nLine: {line}");
+
+        let characters_vector: Vec<char> = line.chars().collect();
+        let mut digit_string = String::default();
+
+        // for all the characters in the line
+        for index in 0..line.len() {
+            // check if a digit is starting at this character
+            let digit_char = get_digit_at_index(&characters_vector, index);
+            
+            match digit_char {
+                Some(digit_char) => {
+                    // if it is, add that digit to a string
+                    digit_string.push(digit_char);
+                }
+                None => {
+                    continue;
+                }
+            }
+        }
+
+        println!("  Parsed into {digit_string}");
+
         // convert the line to a char iterator
         // we filter out every character that is not numeric
-        let digits = line
-            .chars()
-            .filter(|character| character.is_numeric());
-
         // collect the digits into a vector to allow indexing
-        let digits_vector: Vec<char> = digits.collect();
+        let digits_vector: Vec<char> = digit_string
+            .chars()
+            .collect();
 
         if digits_vector.is_empty() {
-            println!("Line: {line}: Line has no digits");
+            println!("  Line has no digits");
             continue;
         }
 
@@ -40,11 +62,72 @@ fn main() {
 
         // parse the new string as an i32
         let calibration_values = first_and_last_digit.parse::<i32>().unwrap();
-        println!("Line: {line}: Calibration values: {calibration_values}");
+        println!("  Calibration values: {calibration_values}");
 
         // add the parsed i32 to the sum
         sum += calibration_values;
     }
 
     println!("Sum: {sum}");
+}
+
+fn get_digit_at_index(characters: &Vec<char>, start_index: usize) -> Option<char> {
+    
+    // this is very inefficient, but I decided to use the same function
+    // for both digits and aliases...
+    // this way, order is easily maintained
+    let digital_aliases = [
+        ("1", '1'),
+        ("2", '2'),
+        ("3", '3'),
+        ("4", '4'),
+        ("5", '5'),
+        ("6", '6'),
+        ("7", '7'),
+        ("8", '8'),
+        ("9", '9'),
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9')
+    ];
+
+    for alias in digital_aliases {
+        let characters_left = characters.len() - start_index;
+        if characters_left < alias.0.len() {
+            // the current alias does not fit in the rest
+            // of the line, so it can't be valid
+            continue;
+        }
+
+        let max_index = start_index + alias.0.len();
+
+        // get a slice of the characters to try matching
+        let slice = &characters[start_index..max_index];
+        let alias_vec: Vec<char> = alias.0.chars().collect();
+
+        let mut alias_matches = true;
+        // iterate over the characters in the alias
+        for index in 0..alias_vec.len() {
+            // if a character does not match, we are invalid
+            if slice[index] != alias_vec[index] {
+                alias_matches = false;
+                break;
+            }
+        }
+
+        if !alias_matches {
+            continue;
+        }
+
+        println!("  Found digit {} at index {}", alias.0, start_index);
+        return Some(alias.1);
+    }
+
+    None
 }
